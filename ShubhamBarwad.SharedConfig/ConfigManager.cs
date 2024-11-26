924853acccbc3cd4358801d6319223105801aa28
Shubham Barwad <sbarwad20595@gmail.com>
2024-11-26 10:37:04 +0530

    TASK/02: Enhacne env parsing

--- ShubhamBarwad.SharedConfig/ConfigManager.cs
+++ ShubhamBarwad.SharedConfig/ConfigManager.cs
@@ -1,14 +1,28 @@
 ﻿namespace ShubhamBarwad.SharedConfig
 {
+    /// <summary>
+    /// Provides a centralized manager for accessing configuration values.
+    /// </summary>
     public class ConfigManager
     {
         private static readonly Dictionary<string, string> _configValues;
 
+        /// <summary>
+        /// Static constructor to initialize the configuration values by loading them from the .env file.
+        /// </summary>
         static ConfigManager()
         {
             _configValues = DotEnvLoader.LoadEnvVariables();
         }
 
+        /// <summary>
+        /// Retrieves the configuration value for the specified key.
+        /// </summary>
+        /// <param name="key">The key of the configuration value to retrieve.</param>
+        /// <returns>The value associated with the specified key.</returns>
+        /// <exception cref="KeyNotFoundException">
+        /// Thrown if the specified key is not found in the configuration.
+        /// </exception>
         public static string GetConfigValue(string key)
         {
             if (_configValues.TryGetValue(key, out var value))
